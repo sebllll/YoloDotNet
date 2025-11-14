05aa15084cd1a546e4b6b59ccd76be34ab5d221b
sebl <sebl@schnellebuntebilder.de>
2025-11-14 16:30:36 +0100

    maxBoundingBoxesToProcess default > 250

--- YoloDotNet/Modules/Interfaces/ISegmentationModule.cs
+++ YoloDotNet/Modules/Interfaces/ISegmentationModule.cs
@@ -14,6 +14,6 @@ namespace YoloDotNet.Modules.Interfaces
         //(ObjectResult[], Texture) ProcessMaskAsTexture(GraphicsDevice device, byte[] imageData, int width, int height, double confidence, double pixelConfidence, double iou, int labelIndex, bool cropToBB, Color4 tint, double scaleBB, bool doRGB, Func<ObjectResult, bool>? bboxFilter);
 
         List<Segmentation> ProcessImage<T>(T image, double confidence, double pixelConfidence, double iou);
-        List<Segmentation> ProcessImageData(byte[] imageData, int width, int height, double confidence, double pixelConfidence, double iou, int labelIndex, bool cropToBB, double scaleBB, Func<ObjectResult, bool>? bboxFilter, int maxBoundingBoxesToProcess = 0);
+        List<Segmentation> ProcessImageData(byte[] imageData, int width, int height, double confidence, double pixelConfidence, double iou, int labelIndex, bool cropToBB, double scaleBB, Func<ObjectResult, bool>? bboxFilter, int maxBoundingBoxesToProcess = 250);
     }
 }
